8c3a069a3dd9870cea46c4cb7f82b9c098f95d63
Timothy Austin <timothyonyea@gmail.com>
2023-01-10 06:55:05 +0100

    added functionailities for User routes and protected routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -5,7 +5,7 @@ const Goal = require('../models/goalModel')
 // @access    Private
 
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find()
+  const goals = await Goal.find({ user: req.user.id})
 
   res.status(200).json(goals);
 });
@@ -20,9 +20,10 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field")
   }
   const goal = await Goal.create({ 
-    text: req.body.text
+    text: req.body.text,
+    user: req.user.id
   })
-  res.status(200).json(goal);
+  res.status(200).json(goal); 
 });
 
 // @desc      Update goal
@@ -39,6 +40,24 @@ const updateGoal = asyncHandler(async (req, res) => {
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
+  
+  if(!goal) {
+    res.status(400)
+    throw new Error ('Goal not found ')
+  }
+
+  const user = await User.findById(req.user.id)
+  
+  // Check for user
+  if(!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+  // Make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
   res.status(200).json(updatedGoal);
 });
 
@@ -52,6 +71,19 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if(!goal) {
     res.status(400)
     throw new Error ('Goal not found ')
+  }
+
+  const user = await User.findById(req.user.id)
+  
+  // Check for user
+  if(!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+  // Make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
   }
     await goal.remove()
   
